d4c3ca713b2162b21123ddfe9f91884d6c413255
csyezheng <csyezheng@gmail.com>
2016-05-03 19:18:35 +0800

    ch10

--- ch10/src/queue.h
+++ ch10/src/queue.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <deque>
+#include <string>
+#include <assert.h>
 
 namespace CLRS
 {
@@ -10,10 +12,12 @@ namespace CLRS
 	public:
 		using size_type = typename Container::size_type;
 		using iterator = typename Container::iterator;
-		Queue(std::size_t sz) : q(sz)
-		{
-			head = tail = q.begin();
-		}
+		using container_type = Container;
+		using value_type = typename Container::value_type;
+		using reference = typename Container::reference;
+		using const_reference = typename Container::const_reference;
+		Queue() : q() { }
+		Queue(std::size_t sz) : q(sz) { }
 		size_type size() const
 		{
 			return q.size();
@@ -24,26 +28,54 @@ namespace CLRS
 		}
 		void enqueue(const T &item)
 		{
+			assert(!check_full_and_increment() && "overflow");
 			q.push_back(item);
-			++tail;
 		}
 		void enqueue(T &&item)
 		{
+			assert(!check_full_and_increment() && "overflow");
 			q.push_back(std::move(item));
-			++tail;
 		}
 		T dequeue()
 		{
-			if (empty())
-				throw std::runtime_error("dequeue on empty queue");
+			assert(!empty() && "underflow");
 			auto ret = q.front();
 			q.pop_front();
 			++head;
 			return ret;
 		}
+		reference front()
+		{
+			assert(!empty() && "front on empty queue");
+			return q.front();
+		}
+		const_reference front() const
+		{
+			assert(!empty() && "front on empty queue");
+			return q.front();
+		}
+		reference back()
+		{
+			assert(!empty() && "back on empty queue");
+			return q.back();
+		}
+		const_reference back() const
+		{
+			assert(!empty() && "back on empty queue");
+			return q.back();
+		}
 	private:
 		Container q;
-		iterator head;
-		iterator tail;
+		size_type head = 0;
+		size_type tail = 0;
+		bool check_full_and_increment() 
+		{
+			if (++tail == q.size() && head == 0)
+				return true;
+			else if (tail == head)
+				return true;
+			else
+				return false;
+		}
 	};
 }
--- ch10/test/queue_test.cpp
+++ ch10/test/queue_test.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include "../src/queue.h"
-/*
+
 int main()
 {
 	CLRS::Queue<int> q;
 	q.enqueue(32);
 	std::cout << q.size() << std::endl;
-	std::cout << q.dequeue() << std::endl;
+	std::cout << q.dequeue() << " " <<  q.front() << " " << q.back() << std::endl;
 	std::cout << q.size() << std::endl;
 	return 0;
 }
-*/
